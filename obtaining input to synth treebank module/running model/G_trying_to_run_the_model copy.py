44f0700eb0b51f056b19897ad43921a2a0d63820
rashiranjan22 <rashi.ranjan.cse22@gmail.com>
2025-11-30 00:06:18 +0530

    added run_trankit_to aid the main file to run on local server

--- obtaining input to synth treebank module/running model/G_trying_to_run_the_model copy.py
+++ obtaining input to synth treebank module/running model/G_trying_to_run_the_model copy.py
@@ -30,30 +30,31 @@
 
 
 
+# ----------------
+
+# %%writefile run_trankit.py
+
+# from trankit import Pipeline
 
 
-from trankit import Pipeline
+# # initialize a multilingual pipeline
+# p = Pipeline(lang='hindi', gpu=True, cache_dir='./cache')
 
 
-# initialize a multilingual pipeline
-p = Pipeline(lang='hindi', gpu=True, cache_dir='./cache')
+# # Tokenizing an English input
+# en_output = p.tokenize(''' इसके अतिरिक्त गुग्गुल कुंड, भीम गुफा तथा भीमशिला भी दर्शनीय स्थल हैं ।''')
+# print(en_output)
+# en_output = p.posdep(''' इसके अतिरिक्त गुग्गुल कुंड, भीम गुफा तथा भीमशिला भी दर्शनीय स्थल हैं ।''')
+# print(en_output)
+# en_output = p.lemmatize(''' इसके अतिरिक्त गुग्गुल कुंड, भीम गुफा तथा भीमशिला भी दर्शनीय स्थल हैं ।''')
+# print(en_output)
 
+# !python run_trankit.py
 
-# Tokenizing an English input
-en_output = p.tokenize(''' इसके अतिरिक्त गुग्गुल कुंड, भीम गुफा तथा भीमशिला भी दर्शनीय स्थल हैं ।''')
-print(en_output)
-en_output = p.posdep(''' इसके अतिरिक्त गुग्गुल कुंड, भीम गुफा तथा भीमशिला भी दर्शनीय स्थल हैं ।''')
-print(en_output)
-en_output = p.lemmatize(''' इसके अतिरिक्त गुग्गुल कुंड, भीम गुफा तथा भीमशिला भी दर्शनीय स्थल हैं ।''')
-print(en_output)
-
-
-
+# ---------------------------------------------
 
 
 # for cleaned data loadin
-
-
 import re
 
 def clean_and_parse_alignment(path, output_path, max_pairs=5000):
